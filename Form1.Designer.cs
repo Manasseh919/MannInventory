26cc0493c14ce80ba7eb1cd2ed24e8f5a71a7037
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2022-08-28 09:28:26 +0000

    I worked on Users and login form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -55,7 +55,7 @@
             this.ts_Login});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(107, 556);
+            this.toolStrip1.Size = new System.Drawing.Size(110, 556);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -65,8 +65,8 @@
             this.ts_stocks.Image = ((System.Drawing.Image)(resources.GetObject("ts_stocks.Image")));
             this.ts_stocks.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.ts_stocks.Name = "ts_stocks";
-            this.ts_stocks.Size = new System.Drawing.Size(104, 59);
-            this.ts_stocks.Text = "Stocks Master";
+            this.ts_stocks.Size = new System.Drawing.Size(107, 59);
+            this.ts_stocks.Text = "Add Item";
             this.ts_stocks.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.ts_stocks.Click += new System.EventHandler(this.ts_stocks_Click);
             // 
@@ -76,7 +76,7 @@
             this.ts_StockOut.Image = ((System.Drawing.Image)(resources.GetObject("ts_StockOut.Image")));
             this.ts_StockOut.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.ts_StockOut.Name = "ts_StockOut";
-            this.ts_StockOut.Size = new System.Drawing.Size(104, 59);
+            this.ts_StockOut.Size = new System.Drawing.Size(121, 59);
             this.ts_StockOut.Text = "Stock-out";
             this.ts_StockOut.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.ts_StockOut.Click += new System.EventHandler(this.ts_StockOut_Click);
@@ -87,7 +87,7 @@
             this.ts_Return.Image = ((System.Drawing.Image)(resources.GetObject("ts_Return.Image")));
             this.ts_Return.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.ts_Return.Name = "ts_Return";
-            this.ts_Return.Size = new System.Drawing.Size(104, 59);
+            this.ts_Return.Size = new System.Drawing.Size(121, 59);
             this.ts_Return.Text = "Stock Return";
             this.ts_Return.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.ts_Return.Click += new System.EventHandler(this.ts_Return_Click);
@@ -98,7 +98,7 @@
             this.ts_ManageUsers.Image = ((System.Drawing.Image)(resources.GetObject("ts_ManageUsers.Image")));
             this.ts_ManageUsers.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.ts_ManageUsers.Name = "ts_ManageUsers";
-            this.ts_ManageUsers.Size = new System.Drawing.Size(104, 59);
+            this.ts_ManageUsers.Size = new System.Drawing.Size(121, 59);
             this.ts_ManageUsers.Text = "Manage Users";
             this.ts_ManageUsers.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.ts_ManageUsers.Click += new System.EventHandler(this.ts_ManageUsers_Click);
@@ -109,8 +109,8 @@
             this.ts_settings.Image = ((System.Drawing.Image)(resources.GetObject("ts_settings.Image")));
             this.ts_settings.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.ts_settings.Name = "ts_settings";
-            this.ts_settings.Size = new System.Drawing.Size(104, 59);
-            this.ts_settings.Text = "Settings";
+            this.ts_settings.Size = new System.Drawing.Size(107, 59);
+            this.ts_settings.Text = "Add  Category";
             this.ts_settings.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.ts_settings.Click += new System.EventHandler(this.ts_settings_Click);
             // 
@@ -120,7 +120,7 @@
             this.ts_Report.Image = ((System.Drawing.Image)(resources.GetObject("ts_Report.Image")));
             this.ts_Report.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.ts_Report.Name = "ts_Report";
-            this.ts_Report.Size = new System.Drawing.Size(104, 59);
+            this.ts_Report.Size = new System.Drawing.Size(121, 59);
             this.ts_Report.Text = "Report";
             this.ts_Report.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.ts_Report.Click += new System.EventHandler(this.ts_Report_Click);
@@ -131,16 +131,16 @@
             this.ts_Login.Image = global::InventorySystem1._0.Properties.Resources.login;
             this.ts_Login.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.ts_Login.Name = "ts_Login";
-            this.ts_Login.Size = new System.Drawing.Size(104, 59);
+            this.ts_Login.Size = new System.Drawing.Size(121, 59);
             this.ts_Login.Text = "Login";
             this.ts_Login.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.ts_Login.Click += new System.EventHandler(this.ts_Login_Click);
             // 
             // statusStrip1
             // 
-            this.statusStrip1.Location = new System.Drawing.Point(107, 534);
+            this.statusStrip1.Location = new System.Drawing.Point(110, 534);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(881, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(878, 22);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
             // 
